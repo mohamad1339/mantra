8013ed29f561722f79565f4c366a58e371fffbe3
mohammad <m.m.noroozi1369@gmail.com>
2023-11-21 14:21:50 +0330

    v5

--- js/script.js
+++ js/script.js
@@ -8,9 +8,13 @@ $(document).ready(function () {
     }
   });
   $("#top").click(function () {
-    $("html,body").animate({
-      scrollTop: 0,
-    },500,("swing"));
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      500,
+      "swing"
+    );
   });
 });
 
@@ -22,4 +26,71 @@ search3.addEventListener("click", function () {
 });
 close.addEventListener("click", function () {
   search1.style.display = "none";
+});
+
+let btn = document.getElementById("ham1");
+let close1 = document.getElementById("close1");
+let menha = document.getElementById("vertical");
+let list = document.getElementById("list1");
+btn.addEventListener("click", function () {
+  list.style.display = "block";
+});
+close1.addEventListener("click", function () {
+  list.style.display = "none";
+});
+let open1 = document.getElementById("open1");
+let list2 = document.getElementById("list2");
+
+open1.addEventListener("click", function () {
+  list2.classList.toggle("opa");
+  menha.classList.toggle("hide");
+});
+let open2 = document.getElementById("open2");
+let list3 = document.getElementById("list3");
+let menha1 = document.getElementById("vertica2");
+
+open2.addEventListener("click", function () {
+  list3.classList.toggle("opa");
+  menha1.classList.toggle("hide");
+});
+let open3 = document.getElementById("open3");
+let list4 = document.getElementById("list4");
+let menha2 = document.getElementById("vertica3");
+
+open3.addEventListener("click", function () {
+  list4.classList.toggle("opa");
+  menha2.classList.toggle("hide");
+});
+let open4 = document.getElementById("open4");
+let list5 = document.getElementById("list5");
+let menha3 = document.getElementById("vertica4");
+
+open4.addEventListener("click", function () {
+  list5.classList.toggle("opa");
+  menha3.classList.toggle("hide");
+});
+let open5 = document.getElementById("open5");
+let list6 = document.getElementById("list6");
+let menha4 = document.getElementById("vertica5");
+
+open5.addEventListener("click", function () {
+  list6.classList.toggle("opa");
+  menha4.classList.toggle("hide");
+});
+let open6 = document.getElementById("open6");
+let list7 = document.getElementById("list7");
+let menha5 = document.getElementById("vertica6");
+
+open6.addEventListener("click", function () {
+  list7.classList.toggle("opa");
+  menha5.classList.toggle("hide");
+});
+let search4 = document.getElementById("search4");
+let search6 = document.getElementById("search6");
+let clos = document.getElementById("clos");
+search6.addEventListener("click", function () {
+  search4.style.display = "block";
+});
+clos.addEventListener("click", function () {
+  search4.style.display = "none";
 });
